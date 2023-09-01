01a249e3f219b431dce0e056bff24ebe9c6fc9db
Adam Laszlo <adam00swim@gmail.com>
2023-09-01 20:57:20 +0200

    most jo

--- js/login.js
+++ js/login.js
@@ -5,6 +5,7 @@ function ellenorizJelszo() {
     if (jelszo === "kpw.etkd") { // Itt add meg a helyes jelszót
         document.getElementById("bejelentkezes").style.display = "none";
         document.getElementById("tartalom").style.display = "block";
+        document.getElementById("gombok").style.display = "flex"; // Gombok megjelenítése
         return false; // Ne küldje el az űrlapot
     } else {
         alert("Hibás jelszó. Kérlek, próbáld újra.");
