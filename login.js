787d78efe1d3ee59993dacacdb5b2192a5f89173
Adam Laszlo <adam00swim@gmail.com>
2023-09-01 17:19:17 +0200

    js

---
+++ login.js
@@ -0,0 +1,14 @@
+
+function ellenorizJelszo() {
+    var jelszo = document.getElementById("jelszo").value;
+
+    // Ellenőrzés
+    if (jelszo === "kpw.etkd") { // Itt add meg a helyes jelszót
+        document.getElementById("bejelentkezes").style.display = "none";
+        document.getElementById("tartalom").style.display = "block";
+        return false; // Ne küldje el az űrlapot
+    } else {
+        alert("Hibás jelszó. Kérlek, próbáld újra.");
+        return false; // Ne küldje el az űrlapot
+    }
+}
