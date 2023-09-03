006175f19ad9180077f1fad8b9bc2be0774b79cf
Adam Laszlo <adam00swim@gmail.com>
2023-09-03 14:58:28 +0200

    sheets update

--- js/button.js
+++ js/button.js
@@ -1,26 +1,35 @@
 function megjelenitSzoveg(gombSzam) {
     var tartalomDiv = document.getElementById("tartalom");
-    var iframe_sheet = document.getElementById("tablazat");
-    var iframe_drive = document.getElementById("drive")
+    var iframe_sheet_friday = document.getElementById("tablazat_pentek");
+    var iframe_sheet_saturday = document.getElementById("tablazat_szombat");
+    var iframe_sheet_sunday = document.getElementById("tablazat_vasarnap")
+
 
     switch (gombSzam) {
         case 1:
-            iframe_sheet.style.display = "block";
-            iframe_drive.style.display = "none";
-            iframe_sheet.src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsdLwFOE9nyXzrTl5ZoXOD08BqlPfpIVq4fUabT9Bj_sgDXl6Wk8m_ouECsNn3wQ/pubhtml?widget=true&amp;headers=false";
+            iframe_sheet_friday.style.display = "block";
+            iframe_sheet_saturday.style.display = "none";
+            iframe_sheet_sunday.style.display = "none";
+            iframe_sheet_friday.src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSqXIL8rxq-i4odH8Q1xmoxQVmr-ik1Flhe5ISfQ3IWSEpuz9B8Px-lEuP8_AzBEA/pubhtml?gid=321834512&amp;single=true&amp;widget=true&amp;headers=false";
             break;
         case 2:
-            iframe_sheet.style.display = "none";
-            iframe_drive.style.display = "block";
-            iframe_drive.src = "https://drive.google.com/embeddedfolderview?id=1hEq_MAF3St2ZljnMKJREASpp1C4kh553#list";
+            iframe_sheet_friday.style.display = "none";
+            iframe_sheet_saturday.style.display = "block";
+            iframe_sheet_sunday.style.display = "none";
+            iframe_sheet_saturday.src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSCfUhZGWiR-FZLODkfTA3ofrnf9KQRnAuUcVdGBszYdhnilrE7xzaQjdtiasmLMw/pubhtml?gid=817917473&amp;single=true&amp;widget=true&amp;headers=false";
             break;
         case 3:
-            // Harmadik gomb szövege és tartalma
+            iframe_sheet_friday.style.display = "none";
+            iframe_sheet_saturday.style.display = "none";
+            iframe_sheet_sunday.style.display = "block";
+            iframe_sheet_sunday.src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_yOVE1sYGwis4QjH5DiHnWrM6wY2HWWPr2wy67QWKk684_b6G44lIs1r3J_fNzQ/pubhtml?gid=2007119411&amp;single=true&amp;widget=true&amp;headers=false";
+
             break;
 
         default:
-            iframe_sheet.style.display = "none";
-            iframe_drive.style.display = "none";
+            iframe_sheet_friday.style.display = "none";
+            iframe_sheet_saturday.style.display = "none";
+            iframe_sheet_sunday.style.display = "none";
             break;
     }
 
